e284087e8cb6eed6136ff7e4252f58a1a2cea9ab
ladoblanco <whitesidekevin@gmail.com>
2023-04-23 15:40:33 -0400

    4/24/23 update - toolbars

--- trevor_sawler_fyne/gold/ui.go
+++ trevor_sawler_fyne/gold/ui.go
@@ -15,8 +15,14 @@ func (app *Config) makeUI() {
 
 	app.PriceContainer = priceContent
 
+	// get toolbar
+	toolBar := app.getToolBar(app.MainWindow) // NOTE: here I'm starting to see the patterns. Even
+	// with the separate files they are all in package 'main' and all connected with methods
+	// off of *Config. This connects everything as all data from all packages are connecting
+	// back to the storage struct and then I have access throughout the app.
+
 	// add container to window
-	finalContent := container.NewVBox(priceContent)
+	finalContent := container.NewVBox(priceContent, toolBar)
 
 	app.MainWindow.SetContent(finalContent)
 
