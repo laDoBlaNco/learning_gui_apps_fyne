980733c742bf853723ecba531299b50863422a20
ladoblanco <whitesidekevin@gmail.com>
2023-05-01 23:45:35 -0400

    Progress update 5/2/23 - Added tabs to Gold

--- trevor_sawler_fyne/gold/ui.go
+++ trevor_sawler_fyne/gold/ui.go
@@ -1,6 +1,10 @@
 package main
 
-import "fyne.io/fyne/v2/container"
+import (
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
+)
 
 func (app *Config) makeUI() {
 	// get the current price of gold - we do this with a separate function that returns
@@ -16,13 +20,21 @@ func (app *Config) makeUI() {
 	app.PriceContainer = priceContent
 
 	// get toolbar
-	toolBar := app.getToolBar(app.MainWindow) // NOTE: here I'm starting to see the patterns. Even
+	toolBar := app.getToolBar() // NOTE: here I'm starting to see the patterns. Even
 	// with the separate files they are all in package 'main' and all connected with methods
 	// off of *Config. This connects everything as all data from all packages are connecting
 	// back to the storage struct and then I have access throughout the app.
+	app.ToolBar = toolBar
+
+	// get app tabs
+	tabs := container.NewAppTabs(
+		container.NewTabItemWithIcon("Prices", theme.HomeIcon(), canvas.NewText("Price content goes here", nil)),
+		container.NewTabItemWithIcon("Holdings", theme.InfoIcon(), canvas.NewText("Holdings content goes here", nil)),
+	)
+	tabs.SetTabLocation(container.TabLocationTop)
 
 	// add container to window
-	finalContent := container.NewVBox(priceContent, toolBar)
+	finalContent := container.NewVBox(priceContent, toolBar, tabs)
 
 	app.MainWindow.SetContent(finalContent)
 
